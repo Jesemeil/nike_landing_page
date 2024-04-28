a00b66d982ff334fe0e9217575a18d6ef1c3f9e7
Jesemeil <otahaghogho@gmail.com>
2024-04-28 16:00:50 +0100

    Added Sections

---
+++ src/components/Nav.jsx
@@ -0,0 +1,21 @@
+import { headerLogo } from "../assets/images";
+import { hamburger } from "../assets/icons";
+const Nav = () => {
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img
+            src={headerLogo}
+            alt="logo"
+            width={129}
+            height={29}
+            className="m-0 w-[129px] h-[29px]"
+          />
+        </a>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
---
+++ src/sections/CustomerReviews.jsx
@@ -0,0 +1,5 @@
+const CustomerReviews = () => {
+  return <div>CustormerReviews</div>;
+};
+
+export default CustomerReviews;
